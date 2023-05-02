f660bca5ea4ba9afcb6db7ca397bd4a90d6b83c6
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2023-05-03 01:36:51 +0500

    read and write files with sort funct.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,55 @@
-import fs from "fs";
-import path from "path";
+import fs from "fs"
 
-function main() {
-  const file = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-  const [list] = file.split("\n");
-
-  const result = list;
-
-  fs.writeFileSync(path.join(__dirname, "output.txt"), result.toString());
+interface Sortable {
+  sort(): void
 }
-main();
+
+class NumberList implements Sortable {
+  numbers: number[]
+
+  constructor(numbers: number[]) {
+    this.numbers = numbers
+  }
+
+  sort(): void {
+    this.numbers.sort((a, b) => b - a)
+  }
+
+  toString(): string {
+    return this.numbers.join(", ")
+  }
+}
+
+class FileNumberList extends NumberList {
+  constructor(private inpfilePath: string, private outpfilePath: string) {
+    super([])
+  }
+
+  read(): void {
+   const fileContent: string = fs.readFileSync(this.inpfilePath, "utf-8")
+   const numbers: number[] = fileContent.split(", ").map(Number)
+   this.numbers = numbers
+  }
+
+  write(): void {
+   const content: string = this.toString()
+   fs.writeFileSync(this.outpfilePath, content)
+  }
+}
+
+function main(): void {
+  const inputFile = "src/input.txt"
+  const outputFile = "src/output.txt"
+  const numberList = new FileNumberList(inputFile, outputFile)
+
+  try {
+    numberList.read()
+    numberList.sort()
+    numberList.write()
+    console.log("Sorted numbers written to output.txt.")
+  } catch (error: any) {
+    console.log(error.message)
+  }
+}
+
+main()
