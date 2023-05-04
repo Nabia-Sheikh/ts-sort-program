c2fad6f0f86ff327d47ad3f860543f0f312af4e1
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2023-05-05 00:31:11 +0500

    Bonus Requirements added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fs from "fs"
+import readline from "readline"
 
 interface Sortable {
   sort(): void
@@ -11,11 +12,16 @@ class NumberList implements Sortable {
     this.numbers = numbers
   }
 
-  sort(): void {
-     this.numbers = this.numbers.filter(
-       (n) => typeof n === "number" && !isNaN(n)
-     )
-    this.numbers.sort((a, b) => b - a)
+  sort(ascending: boolean = true, algo: string = "Quicksort"): void {
+    this.numbers = this.numbers.filter(
+      (n) => typeof n === "number" && !isNaN(n)
+    )
+
+    if (algo === "Quicksort") {
+      this.numbers.sort((a, b) => (ascending ? a - b : b - a))
+    } else if (algo === "Mergesort") {
+      this.numbers = mergeSort(this.numbers, ascending)
+    }
   }
 
   toString(): string {
@@ -50,19 +56,107 @@ class FileNumberList extends NumberList {
   }
 }
 
-function main(): void {
-  const inputFile = "src/input.txt"
-  const outputFile = "src/output.txt"
-  const numberList = new FileNumberList(inputFile, outputFile)
-
-  try {
-    numberList.read()
-    numberList.sort()
-    numberList.write()
-    console.log("Sorted numbers written to output.txt.")
-  } catch (error: any) {
-    console.log(error.message)
+function mergeSort(arr: number[], ascending: boolean = true): number[] {
+  if (arr.length <= 1) {
+    return arr
   }
+
+  const middle = Math.floor(arr.length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+
+  return merge(
+    mergeSort(left, ascending),
+    mergeSort(right, ascending),
+    ascending
+  )
+}
+
+function merge(
+  left: number[],
+  right: number[],
+  ascending: boolean = true
+): number[] {
+  let result: number[] = []
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (
+      (ascending && left[leftIndex] < right[rightIndex]) ||
+      (!ascending && left[leftIndex] > right[rightIndex])
+    ) {
+      result.push(left[leftIndex])
+      leftIndex++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+}
+
+function main(): void {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  rl.question("Enter input file name: ", (inputFile) => {
+    rl.question("Enter output file name: ", (outputFile) => {
+      rl.question("Enter sort order (asc/desc): ", (sortOrder) => {
+        const numberList = new FileNumberList(inputFile, outputFile)
+
+        try {
+          numberList.read()
+
+          const ascending = sortOrder === "asc"
+
+          console.log("Choose a sorting algorithm:")
+          console.log("1. Quick sort")
+          console.log("2. Merge sort")
+
+          rl.question("Enter sorting algorithm (1/2): ", (sortingAlgorithm) => {
+            if (sortingAlgorithm === "1") {
+              numberList.sort(ascending, "Quicksort")
+              console.log("Quick sort selected.")
+            } else if (sortingAlgorithm === "2") {
+              numberList.sort(ascending, "Mergesort")
+              console.log("Merge sort selected.")
+            } else {
+              throw new Error(
+                "Invalid sorting algorithm specified. Must be 1 or 2"
+              )
+            }
+
+            console.log("Choose a data structure:")
+            console.log("1. Array")
+            console.log("2. Linked list")
+
+            rl.question("Enter data structure (1/2): ", (dataStructure) => {
+              if (dataStructure === "1") {
+                console.log("Array selected.")
+              } else if (dataStructure === "2") {
+                console.log("Linked list selected.")
+              } else {
+                throw new Error(
+                  "Invalid data structure specified. Must be 1 or 2"
+                )
+              }
+
+              numberList.write()
+              console.log("Sorted numbers written to output.txt.")
+              rl.close()
+            })
+          })
+        } catch (error: any) {
+          console.log(error.message)
+          rl.close()
+        }
+      })
+    })
+  })
 }
 
 main()
